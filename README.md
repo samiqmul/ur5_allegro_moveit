cc805d0889da91eb7e6af3fe549b1b3605fcb1ee
samiqmul <m.s.siddiqui@qmul.ac.uk>
2020-10-14 16:35:45 +0100

    oct2020

--- README.md
+++ README.md
@@ -4,15 +4,21 @@ Moveit package for ur5 &amp; allegro combined.
 You can launch the MoveIt! and RViz test using:
 
 ```
-roslaunch ur5_allegro_moveit demo.launch
+roslaunch ur5_allegro_moveit demo.launch optoforce:=true
 ```
 
-## Dependances
+This package is meant to be used with optoforce sensors, but you can disable them via the launch parameter _optoforce_. In that case MoveIt will give warnings that some links in the collision matrix (e.g. omd_1) does not exist. These warnings can be ignored.
 
-In order to use this package, the following packages should be installed:
+## Install
+*Tested on Ubuntu 16.04, ROS-Kinetic*
+
+In order to use this package, the following should be installed:
 
 * [MoveIt!](https://moveit.ros.org/)
-* [allegro-hand-ros](https://github.com/gokhansolak/allegro-hand-ros)
+* [*allegro-hand-ros (experimental branch)*](https://github.com/gokhansolak/allegro-hand-ros/tree/experimental)
 * [universal-robot](https://github.com/ros-industrial/universal_robot)
 
-The current ROS distro is ***kinetic***.
+**Optionally**, if you use Allegro with optoforce sensors:
+* [optoforce-publisher](https://github.com/ARQ-CRISP/optoforce-publisher)
+
+Most of these packages are installed for accessing the _xacro_ and _urdf_ files they contain.
--- src/js_merger_node.cpp
+++ src/js_merger_node.cpp
@@ -1,3 +1,6 @@
+// Copyright (C) 2020  CRISP, Queen Mary University of London
+// This code is licensed under the BSD 3-Clause license (see LICENSE for details)
+
 
 #include <ros/ros.h>
 
@@ -38,7 +41,7 @@ void publishMerged(){
   // add joints from allegro
   int size_allegro = js_allegro_.name.size();
   for (int j=0; j<size_allegro; j++){
-    
+
     if(overwrite_){
       js_merged.position[j] = js_allegro_.position[j];
       js_merged.velocity[j] = js_allegro_.velocity[j];
@@ -78,6 +81,45 @@ void allegroCallback(const sensor_msgs::JointState::ConstPtr &msg_in) {
   publishMerged();
 }
 
+// this is needed to avoid waiting for ur5 joint states
+// which is not published until a moveit plan is executed
+bool getZerosParams() {
+
+  string ns_zeros = "/ur5_allegro_moveit/zeros/";
+
+  if(ros::param::has(ns_zeros)){
+    // zeros param is only used in sim, it should not be set in real-world
+    ROS_WARN_STREAM(ns_zeros<<" is found. It should only be loaded in simulation.");
+  }
+
+  // ur5 joints
+  js_ur5_.name.push_back("shoulder_pan_joint");
+  js_ur5_.name.push_back("shoulder_lift_joint");
+  js_ur5_.name.push_back("elbow_joint");
+  js_ur5_.name.push_back("wrist_1_joint");
+  js_ur5_.name.push_back("wrist_2_joint");
+  js_ur5_.name.push_back("wrist_3_joint");
+
+  js_ur5_.position.resize(js_ur5_.name.size());
+  js_ur5_.velocity.resize(js_ur5_.name.size());
+  js_ur5_.effort.resize(js_ur5_.name.size());
+
+  for(int j=0; j<js_ur5_.name.size(); j++)
+    ros::param::get(ns_zeros+js_ur5_.name[j], js_ur5_.position[j]);
+
+  // allegro joints
+  js_allegro_.position.resize(16);
+  js_allegro_.velocity.resize(16);
+  js_allegro_.effort.resize(16);
+  for(int j=0; j<16; j++){
+    string joint_name = "joint_"+to_string(j);
+    js_allegro_.name.push_back(joint_name);
+    ros::param::get(ns_zeros+joint_name, js_allegro_.position[j]);
+  }
+
+  return true;
+}
+
 
 int main(int argc, char **argv){
 
@@ -88,6 +130,8 @@ int main(int argc, char **argv){
 
   // get overwrite param
   ros::param::get("~overwrite", overwrite_);
+  
+  getZerosParams();
 
   ros::Subscriber sub_ur5 = nh.subscribe<sensor_msgs::JointState>( "ur5_js", 1, &ur5Callback);
   ros::Subscriber sub_allegro = nh.subscribe<sensor_msgs::JointState>( "allegro_js", 1, &allegroCallback);
